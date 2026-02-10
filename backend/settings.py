445af00699548f66652d4cb5c7c9711a98c8f192
Iqrazafar0 <iqraraozafar0@.in>
2026-02-10 18:51:11 +0500

    fixed email logic

--- backend/settings.py
+++ backend/settings.py
@@ -135,10 +135,10 @@ CORS_ALLOW_ALL_ORIGINS = True
 # --- EMAIL CONFIGURATION ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True  
-EMAIL_TIMEOUT = 10
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False  
+EMAIL_TIMEOUT = 30
 
 # Render ke Variables ke naam match karein
 EMAIL_HOST_USER = os.getenv('EMAIL_USER') 
